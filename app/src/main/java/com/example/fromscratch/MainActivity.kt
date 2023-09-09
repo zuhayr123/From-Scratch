41a462deedb9d96c3bec4ce1d29d9e177be6d7d4
abou5 <abou@laaltentech.com>
2023-09-10 04:52:23 +0530

    first commit

--- app/src/main/java/com/example/fromscratch/MainActivity.kt
+++ app/src/main/java/com/example/fromscratch/MainActivity.kt
@@ -2,12 +2,10 @@ package com.example.fromscratch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.annotationprocessor.TestClassBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var testClassBuilder = TestClassBuilder() //Created by the annotation processor
     }
 }
--- app/src/main/java/com/example/fromscratch/TestClass.java
+++ app/src/main/java/com/example/fromscratch/TestClass.java
@@ -1,7 +1,4 @@
 package com.example.fromscratch;
 
-import com.example.annotationprocessor.GenerateClass;
-
-@GenerateClass(name = "TestClassBuilder")
 public class TestClass {
 }
