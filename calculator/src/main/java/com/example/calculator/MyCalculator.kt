1f57e97ff6c4534e8b926a751bdda565288fcd81
abou5 <abou@laaltentech.com>
2023-09-12 12:26:51 +0530

    added a second library for calculator

---
+++ calculator/src/main/java/com/example/calculator/MyCalculator.kt
@@ -0,0 +1,16 @@
+package com.example.calculator
+
+class MyCalculator {
+
+    fun addNumbers(a: Int, b: Int): Int{
+        return a+b
+    }
+
+    fun subtractNumbers(a: Int, b: Int): Int{
+        return a-b
+    }
+
+    fun multiplyNumbers(a: Int, b: Int): Int{
+        return a*b
+    }
+}
