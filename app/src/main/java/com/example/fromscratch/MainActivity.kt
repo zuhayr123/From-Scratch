a6cd471f54ee9a718dfd289aecd1ed021d4db122
abou5 <abou@laaltentech.com>
2023-09-12 13:14:58 +0530

    successful build

--- app/src/main/java/com/example/fromscratch/MainActivity.kt
+++ app/src/main/java/com/example/fromscratch/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.fromscratch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.example.calculator.MyCalculator
 import com.example.testlibrary.TestClass
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val testClass = TestClass(1,1).add()
+        Toast.makeText(this, "Result is $testClass", Toast.LENGTH_SHORT).show()
+
+        val result = MyCalculator().addNumbers(1,1)
         Toast.makeText(this, "Result is $testClass", Toast.LENGTH_SHORT).show()
     }
 }
